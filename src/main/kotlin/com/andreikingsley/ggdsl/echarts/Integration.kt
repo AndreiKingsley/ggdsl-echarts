56c8c7d23791068aece1b2cb2dacc203b23c114d
Andrei Kislitsyn <kisandy@yandex.ru>
2021-12-08 17:40:44 +0300

    js

--- src/main/kotlin/com/andreikingsley/ggdsl/echarts/Integration.kt
+++ src/main/kotlin/com/andreikingsley/ggdsl/echarts/Integration.kt
@@ -6,6 +6,9 @@ import org.jetbrains.kotlinx.jupyter.api.libraries.*
 
 import kotlinx.html.*
 import kotlinx.html.stream.createHTML
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 @JupyterLibrary
 internal class Integration : JupyterIntegration() {
@@ -17,8 +20,27 @@ internal class Integration : JupyterIntegration() {
     }
 }
 
-private fun Option.toHTML(): String {
+
+@ExperimentalSerializationApi
+fun Option.toJSON(): String {
+    return Json {
+        explicitNulls = false
+        encodeDefaults = true
+    }.encodeToString(this)
+}
+
+fun Option.toHTML(): String {
     return createHTML().div {
-        style = "width: 100px;height:800px;background: red"
+        div {
+            id = "main" // TODO!!!
+            style = "width: 600;height:400;background: red"
+        }
+        script { src = "https://cdn.jsdelivr.net/npm/echarts@5.2.2/dist/echarts.min.js" }
+        script {
+            type = "text/javascript"
+            +("\n        var myChart = echarts.init(document.getElementById('main'));\n" +
+                    "        var option = ${toJSON()};\n" +
+                    "        myChart.setOption(option);")
+        }
     }
 }
